ab5d3c3590e8ce1912c30758735e0b7de19e65ae
Trịnh Duy Hưng <57101685+Hung-00@users.noreply.github.com>
2025-06-25 14:18:08 +0700

    Update notebook

--- sm-notebook/code/inference.py
+++ sm-notebook/code/inference.py
@@ -2,11 +2,13 @@ import numpy as np
 import torch, os, json, base64, cv2, time
 from ultralytics import YOLO
 
+
 def model_fn(model_dir):
     print("Executing model_fn from inference.py ...")
     env = os.environ
-    model = YOLO(os.path.join(model_dir, env['YOLOV8_MODEL']))
+    model = YOLO(os.path.join(model_dir, env["YOLO11_MODEL"]))
     return model
+
 
 def input_fn(request_body, request_content_type):
     print("Executing input_fn from inference.py ...")
@@ -17,25 +19,27 @@ def input_fn(request_body, request_content_type):
     else:
         raise Exception("Unsupported content type: " + request_content_type)
     return img
-    
+
+
 def predict_fn(input_data, model):
     print("Executing predict_fn from inference.py ...")
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     with torch.no_grad():
         result = model(input_data)
     return result
-        
+
+
 def output_fn(prediction_output, content_type):
     print("Executing output_fn from inference.py ...")
     infer = {}
     for result in prediction_output:
-        if 'boxes' in result._keys and result.boxes is not None:
-            infer['boxes'] = result.boxes.cpu().numpy().data.tolist()
-        if 'masks' in result._keys and result.masks is not None:
-            infer['masks'] = result.masks.cpu().numpy().data.tolist()
-        if 'keypoints' in result._keys and result.keypoints is not None:
-            infer['keypoints'] = result.keypoints.cpu().numpy().data.tolist()
-        if 'probs' in result._keys and result.probs is not None:
-            infer['probs'] = result.probs.cpu().numpy().data.tolist()
+        if "boxes" in result._keys and result.boxes is not None:
+            infer["boxes"] = result.boxes.cpu().numpy().data.tolist()
+        if "masks" in result._keys and result.masks is not None:
+            infer["masks"] = result.masks.cpu().numpy().data.tolist()
+        if "keypoints" in result._keys and result.keypoints is not None:
+            infer["keypoints"] = result.keypoints.cpu().numpy().data.tolist()
+        if "probs" in result._keys and result.probs is not None:
+            infer["probs"] = result.probs.cpu().numpy().data.tolist()
     return json.dumps(infer)
